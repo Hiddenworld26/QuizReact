cca06569ad4442ee07e550d8777fc9265c07bb4b
Shruti Verma <vshruti186@gmail.com>
2024-07-08 18:53:58 +0530

    Quiz React

--- src/QuizApp.jsx
+++ src/QuizApp.jsx
@@ -1,47 +1,49 @@
-
 import React, { useState } from 'react';
 import './QuizApp.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { FaChevronRight, FaChevronLeft } from 'react-icons/fa';
 
 const questions = [
   {
-    q: '1). What is the capital of France?',
-    a: 'Paris',
-    b: 'London',
-    c: 'Berlin',
-    d: 'Madrid',
+    q: '1). What is the national flower of India?',
+    a: 'Lotus',
+    b: 'Rose',
+    c: 'Sunflower',
+    d: 'Hibiscus',
     correct: 'a',
   },
   {
-    q: '2). Which planet is known as the Red Planet?',
-    a: 'Earth',
-    b: 'Mars',
-    c: 'Jupiter',
-    d: 'Saturn',
+    q: '2). What is the capital city of India?',
+    a: 'New Delhi',
+    b: 'Mumbai',
+    c: 'Kolkata',
+    d: 'Chennai',
+    correct: 'a',
+  },
+  {
+    q: '3). Which is the national sport of India?',
+    a: 'Cricket',
+    b: 'Hockey',
+    c: 'Kabadi',
+    d: 'Football',
     correct: 'b',
   },
   {
-    q: '3). Who wrote "To Kill a Mockingbird"?',
-    a: 'Harper Lee',
-    b: 'Mark Twain',
-    c: 'Ernest Hemingway',
-    d: 'F. Scott Fitzgerald',
+    q: '4). Who is popularly known as the “Iron Man” of India?',
+    a: 'Lal Bahadur Shastri',
+    b: 'Sardar Vallabh Bhai Patel',
+    c: 'Mahatma Gandhi',
+    d: 'Dr. B.R Ambedkar',
+    correct: 'b',
+  },
+  {
+    q: '5). Who discovered India?',
+    a: 'Vasco da Gama',
+    b: 'Christopher Columbus',
+    c: 'James Cook',
+    d: 'Willem Janszoon',
     correct: 'a',
-  },
-  {
-    q: '4). What is the largest ocean on Earth?',
-    a: 'Atlantic Ocean',
-    b: 'Indian Ocean',
-    c: 'Arctic Ocean',
-    d: 'Pacific Ocean',
-    correct: 'd',
-  },
-  {
-    q: '5). Who painted the Mona Lisa?',
-    a: 'Vincent van Gogh',
-    b: 'Pablo Picasso',
-    c: 'Leonardo da Vinci',
-    d: 'Claude Monet',
-    correct: 'c',
   },
 ];
 
@@ -56,14 +58,51 @@ const QuizApp = () => {
   };
 
   const handleSubmit = () => {
-    if (selectedOption === questions[index].correct) {
-      setScore(score + 1);
+    if (!selectedOption) {
+      toast.error('Please select an option!', {
+        position: 'top-right',
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+      return;
     }
+
+    
+
     setSelectedOption('');
     if (index < questions.length - 1) {
       setIndex(index + 1);
     } else {
       setShowScore(true);
+      if(score==5)
+      {
+        toast.success('All answers are  Right!', {
+            position: 'top-right',
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+          });
+      }
+
+      else
+      {
+        toast.error(`${score} are correct!`, {
+            position: 'top-right',
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+          });
+      }
     }
   };
 
@@ -81,9 +120,7 @@ const QuizApp = () => {
 
   return (
     <div className="quiz-container">
-      <header className={`header ${showScore ? 'down' : ''}`}>
-        <h1>Random Quiz Challenge!</h1>
-      </header>
+      <ToastContainer />
       {showScore ? (
         <div className="score-container">
           <h3 className="score-text">Hi, you've scored {score} / {questions.length}</h3>
@@ -93,36 +130,71 @@ const QuizApp = () => {
           <div className="container">
             <h2 id="question">{questions[index].q}</h2>
             <div className="options">
-              <input type="radio" value="a" id="option1" checked={selectedOption === 'a'} onChange={handleOptionChange} />
-              <label htmlFor="option1">{questions[index].a}</label>
+              <input
+                type="radio"
+                value="a"
+                id="option1"
+                checked={selectedOption === 'a'}
+                onChange={handleOptionChange}
+              />
+              <label htmlFor="option1">
+                <span className="circle"><FaChevronRight /></span> {questions[index].a}
+              </label>
             </div>
             <div className="options">
-              <input type="radio" value="b" id="option2" checked={selectedOption === 'b'} onChange={handleOptionChange} />
-              <label htmlFor="option2">{questions[index].b}</label>
+              <input
+                type="radio"
+                value="b"
+                id="option2"
+                checked={selectedOption === 'b'}
+                onChange={handleOptionChange}
+              />
+              <label htmlFor="option2">
+                <span className="circle"><FaChevronRight /></span> {questions[index].b}
+              </label>
             </div>
             <div className="options">
-              <input type="radio" value="c" id="option3" checked={selectedOption === 'c'} onChange={handleOptionChange} />
-              <label htmlFor="option3">{questions[index].c}</label>
+              <input
+                type="radio"
+                value="c"
+                id="option3"
+                checked={selectedOption === 'c'}
+                onChange={handleOptionChange}
+              />
+              <label htmlFor="option3">
+                <span className="circle"><FaChevronRight /></span> {questions[index].c}
+              </label>
             </div>
             <div className="options">
-              <input type="radio" value="d" id="option4" checked={selectedOption === 'd'} onChange={handleOptionChange} />
-              <label htmlFor="option4">{questions[index].d}</label>
+              <input
+                type="radio"
+                value="d"
+                id="option4"
+                checked={selectedOption === 'd'}
+                onChange={handleOptionChange}
+              />
+              <label htmlFor="option4">
+                <span className="circle"><FaChevronRight /></span> {questions[index].d}
+              </label>
             </div>
 
             <div className="button-container">
-              {index > 0 && index < questions.length - 1 && 
-                <button className="btn" onClick={handlePrevious}>Previous</button>
-              }
-              {index === questions.length - 1 &&
-                <button className="btn-center" onClick={handlePrevious}>Previous</button>
-              }
-              {index < questions.length - 1 &&
-                <button className={`btn ${index === 0 ? 'btn-center' : ''}`} onClick={handleNext}>Next</button>
-              }
-              {index === questions.length - 1 && (
-                <button className="btn-submit" onClick={handleSubmit}>Submit</button>
+              {index !== 0 && (
+                <button className="btn btn-center" onClick={handlePrevious}>
+                  <FaChevronLeft /> 
+                </button>
               )}
+              {index !== questions.length - 1 && (
+                <button className="btn btn-center" onClick={handleNext}>
+                 <FaChevronRight />
+                </button>
+              )}
+             
             </div>
+
+            <button className="btn btn-submit" onClick={handleSubmit}>
+                Submit
+              </button>
           </div>
         </main>
       )}
