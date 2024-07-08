f5cd0ace700027641f4424a25ca98d45ba45ba86
Shruti Verma <vshruti186@gmail.com>
2024-07-08 18:02:58 +0530

    quiz app

--- src/QuizApp.jsx
+++ src/QuizApp.jsx
@@ -1,47 +1,47 @@
-// QuizApp.js
+
 import React, { useState } from 'react';
 import './QuizApp.css';
 
 const questions = [
   {
-    q: '1). What is the national flower of India?',
-    a: 'Lotus',
-    b: 'Rose',
-    c: 'Sunflower',
-    d: 'Hibiscus',
+    q: '1). What is the capital of France?',
+    a: 'Paris',
+    b: 'London',
+    c: 'Berlin',
+    d: 'Madrid',
     correct: 'a',
   },
   {
-    q: '2). What is the capital city of India?',
-    a: 'New Delhi',
-    b: 'Mumbai',
-    c: 'Kolkata',
-    d: 'Chennai',
-    correct: 'a',
-  },
-  {
-    q: '3). Which is the national sport of India?',
-    a: 'Cricket',
-    b: 'Hockey',
-    c: 'Kabadi',
-    d: 'Football',
+    q: '2). Which planet is known as the Red Planet?',
+    a: 'Earth',
+    b: 'Mars',
+    c: 'Jupiter',
+    d: 'Saturn',
     correct: 'b',
   },
   {
-    q: '4). Who is popularly known as the “Iron Man” of India?',
-    a: 'Lal Bahadur Shastri',
-    b: 'Sardar Vallabh Bhai Patel',
-    c: 'Mahatma Gandhi',
-    d: 'Dr. B.R Ambedkar',
-    correct: 'b',
+    q: '3). Who wrote "To Kill a Mockingbird"?',
+    a: 'Harper Lee',
+    b: 'Mark Twain',
+    c: 'Ernest Hemingway',
+    d: 'F. Scott Fitzgerald',
+    correct: 'a',
   },
   {
-    q: '5). Who discovered India?',
-    a: 'Vasco da Gama',
-    b: 'Christopher Columbus',
-    c: 'James Cook',
-    d: 'Willem Janszoon',
-    correct: 'a',
+    q: '4). What is the largest ocean on Earth?',
+    a: 'Atlantic Ocean',
+    b: 'Indian Ocean',
+    c: 'Arctic Ocean',
+    d: 'Pacific Ocean',
+    correct: 'd',
+  },
+  {
+    q: '5). Who painted the Mona Lisa?',
+    a: 'Vincent van Gogh',
+    b: 'Pablo Picasso',
+    c: 'Leonardo da Vinci',
+    d: 'Claude Monet',
+    correct: 'c',
   },
 ];
 
@@ -80,12 +80,10 @@ const QuizApp = () => {
   };
 
   return (
-    <>
-    <header className="header">
-        <h1>Know More About India!</h1>
-      </header>
     <div className="quiz-container">
-      
+      <header className={`header ${showScore ? 'down' : ''}`}>
+        <h1>Random Quiz Challenge!</h1>
+      </header>
       {showScore ? (
         <div className="score-container">
           <h3 className="score-text">Hi, you've scored {score} / {questions.length}</h3>
@@ -112,21 +110,24 @@ const QuizApp = () => {
             </div>
 
             <div className="button-container">
-              {index > 0 && 
+              {index > 0 && index < questions.length - 1 && 
                 <button className="btn" onClick={handlePrevious}>Previous</button>
               }
-              {index < questions.length - 1 &&
-                <button className="btn" onClick={handleNext}>Next</button>
+              {index === questions.length - 1 &&
+                <button className="btn-center" onClick={handlePrevious}>Previous</button>
               }
-              <button className="btn-submit" onClick={handleSubmit}>Submit</button>
+              {index < questions.length - 1 &&
+                <button className={`btn ${index === 0 ? 'btn-center' : ''}`} onClick={handleNext}>Next</button>
+              }
+              {index === questions.length - 1 && (
+                <button className="btn-submit" onClick={handleSubmit}>Submit</button>
+              )}
             </div>
           </div>
         </main>
       )}
     </div>
-    </>
   );
-  
 };
 
 export default QuizApp;
